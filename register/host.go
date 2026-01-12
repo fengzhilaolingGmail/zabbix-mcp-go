c89edf683f28c734b5f46dd5704d512de07d363a
fengzhilaoling <fengzhilaoling@gmail.com>
2026-01-12 13:31:28 +0800

    fix: 重构主机查询接口

--- register/host.go
+++ register/host.go
@@ -2,7 +2,7 @@
  * @Author: fengzhilaoling fengzhilaoling@gmail.com
  * @Date: 2026-01-02 15:33:32
  * @LastEditors: fengzhilaoling
- * @LastEditTime: 2026-01-08 13:57:08
+ * @LastEditTime: 2026-01-12 13:28:45
  * @FilePath: \zabbix-mcp-go\register\host.go
  * @Description: 文件解释
  * Copyright (c) 2026 by fengzhilaoling@gmail.com, All Rights Reserved.
@@ -16,42 +16,197 @@ import (
 	"github.com/mark3labs/mcp-go/server"
 )
 
-func registerHost(s *server.MCPServer) {
+func registerGetHost(s *server.MCPServer) {
 	s.AddTool(
-		mcp.NewTool("get_hosts",
+		mcp.NewTool("get_host_by_name",
 			mcp.WithDescription("获取实例Zabbix主机信息,支持所有或模糊匹配"),
+			mcp.WithString("hostnames", mcp.Required(), mcp.Description("主机名称列表")),
 			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
 			mcp.WithString("active_available", mcp.Description("主机状态,0-未知,1-在线,2-离线,默认为1")),
 			mcp.WithString("type", mcp.Description("主机类型,1 - Agent 2 - SNMP 3 - IPMI 4 - JMX,默认为1")),
 		),
-		handler.GetHostsHandler,
+		handler.GetHostForNameHandler,
 	)
+	// 获取主机列表的图形
 	s.AddTool(
-		mcp.NewTool("find_host_by_name", mcp.WithDescription("通过主机名称获取主机信息,模糊匹配需要启用search参数"),
+		mcp.NewTool("get_host_graphs", mcp.WithDescription("获取主机列表的图形"),
 			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
-			mcp.WithArray("hostnames", mcp.Required(), mcp.Description("主机名称列表")),
-			mcp.WithBoolean("select_discoveries", mcp.Description("是否查询主机低级发现规则")), // ok
-			mcp.WithBoolean("select_discovery_data", mcp.Description("是否查询主机发现数据")),
-			mcp.WithBoolean("select_discovery_rule", mcp.Description("是否查询主机低级发现规则的rule")),
-			mcp.WithBoolean("select_discovery_rule_prototype", mcp.Description("是否查询主机低级发现规则原型")),
-			mcp.WithBoolean("select_graphs", mcp.Description("是否查询主机图形")), // ok
-			mcp.WithBoolean("select_host_discovery", mcp.Description("是否查询主机发现数据")),
-			mcp.WithBoolean("select_host_groups", mcp.Description("是否查询主机组")),
-			mcp.WithBoolean("select_http_tests", mcp.Description("是否查询主机Web检查")),
-			mcp.WithBoolean("select_interfaces", mcp.Description("是否查询主机接口")),
-			mcp.WithBoolean("select_inventory", mcp.Description("是否查询主机清单数据")),
-			mcp.WithBoolean("select_items", mcp.Description("是否查询主机监控项")),
-			mcp.WithBoolean("select_macros", mcp.Description("是否查询主机宏")),
-			mcp.WithBoolean("select_parent_templates", mcp.Description("是否查询主机模板")), // ok
-			mcp.WithBoolean("select_dashboards", mcp.Description("是否查询主机仪表盘")),
-			mcp.WithBoolean("select_tags", mcp.Description("是否查询主机标签")),
-			mcp.WithBoolean("select_inherited_tags", mcp.Description("是否查询主机继承标签")),
-			mcp.WithBoolean("select_triggers", mcp.Description("是否查询主机触发器")),
-			mcp.WithBoolean("select_value_maps", mcp.Description("是否查询主机值映射")),
-			mcp.WithBoolean("search", mcp.Description("是否启用模糊搜索")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:graphs 表示获取图形")),
 		),
 		handler.GetHostsHandler,
 	)
+	// 获取主机列表的图形
+	s.AddTool(
+		mcp.NewTool("get_host_templates", mcp.WithDescription("获取主机列表的模板"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:templates 表示获取模板")),
+		),
+		handler.GetHostsHandler,
+	)
+	// 获取主机列表的监控项
+	s.AddTool(
+		mcp.NewTool("get_host_items", mcp.WithDescription("获取主机列表的监控项"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:items 表示获取监控项")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_triggers", mcp.WithDescription("获取主机列表的触发器"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:triggers 表示获取触发器")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_macros", mcp.WithDescription("获取主机列表的宏"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:macros 表示获取宏")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_value_maps", mcp.WithDescription("获取主机列表的数值映射"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:value_maps 表示获取数值映射")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_groups", mcp.WithDescription("获取主机列表的主机组"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:groups 表示获取主机组")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_tags", mcp.WithDescription("获取主机列表的标签"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:tags 表示获取标签")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_dashboards", mcp.WithDescription("获取主机列表的仪表盘"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:dashboards 表示获取仪表盘")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_discoveries", mcp.WithDescription("获取主机列表的低级发现规则"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:discoveries 表示获取低级发现规则")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_discovery_rule", mcp.WithDescription("获取主机列表的发现事件列表"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:discovery_rule 表示获取发现事件列表")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_discovery_data", mcp.WithDescription("获取主机列表的最近一次发现数据"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:discovery_data 表示获取最近一次发现数据")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_discovery_rule_prototype", mcp.WithDescription("获取主机列表的发现规则原型配置"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:discovery_rule_prototype 表示获取发现规则原型配置")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_discovery", mcp.WithDescription("获取主机列表的发现记录"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:discovery 表示获取最近一次发现数据")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_http_tests", mcp.WithDescription("获取主机列表的Web检查"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:http_tests 表示获取主机Web检查")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_interfaces", mcp.WithDescription("获取主机列表的接口"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:interfaces 表示获取主机接口")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_inventory", mcp.WithDescription("获取主机列表的清单数据"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:inventory 表示获取主机清单数据")),
+		),
+		handler.GetHostsHandler,
+	)
+	s.AddTool(
+		mcp.NewTool("get_host_inherited_tags", mcp.WithDescription("获取主机列表的继承标签"),
+			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表,数量不要超过4个")),
+			mcp.WithString("style", mcp.Required(), mcp.Description("默认:inherited_tags 表示获取主机继承标签")),
+		),
+		handler.GetHostsHandler,
+	)
+}
+
+func registerHost(s *server.MCPServer) {
+
+	// s.AddTool( macros
+	// 	mcp.NewTool("find_host_by_name", mcp.WithDescription("通过主机名称获取主机信息,模糊匹配需要启用search参数"),
+	// 		mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
+	// 		mcp.WithArray("hostnames", mcp.Required(), mcp.Description("主机名称列表")),
+	// 		mcp.WithBoolean("select_host_discovery", mcp.Description("是否查询主机发现数据")),
+	// 		mcp.WithBoolean("select_http_tests", mcp.Description("是否查询主机Web检查")),
+	// 		mcp.WithBoolean("select_interfaces", mcp.Description("是否查询主机接口")),
+	// 		mcp.WithBoolean("select_inventory", mcp.Description("是否查询主机清单数据")),
+	// 		mcp.WithBoolean("select_inherited_tags", mcp.Description("是否查询主机继承标签")),
+	// 	),
+	// 	handler.GetHostsHandler,
+	// )
 	s.AddTool(
 		mcp.NewTool("create_host",
 			mcp.WithDescription("在指定实例中创建主机"),
@@ -95,15 +250,5 @@ func registerHost(s *server.MCPServer) {
 			mcp.WithObject("inventory", mcp.Description("主机资产清单对象，用于替换清单")),
 		),
 		handler.UpdateHostHandler,
-	)
-	//
-	s.AddTool(
-		mcp.NewTool("get_host_graphs", mcp.WithDescription("获取主机列表的图形"),
-			mcp.WithString("instance", mcp.Required(), mcp.Description("Zabbix实例名称必须填")),
-			mcp.WithArray("hostids", mcp.Required(), mcp.Description("主机ID列表")),
-			mcp.WithBoolean("is_detailed", mcp.Required(), mcp.Description("是否查询详细信息: 默认 false")),
-			mcp.WithString("style", mcp.Required(), mcp.Description("默认:graphs 表示获取图形")),
-		),
-		handler.GetHostsRefineHandler,
 	)
 }
